762248de592366b602f52336aa2a2e8fa44891bd
iamchristopher <chriswright@live.com>
2015-07-12 22:07:08 -0400

    Added sample grid background tile

--- source/script/app/state/Game.js
+++ source/script/app/state/Game.js
@@ -9,6 +9,12 @@ define(['phaser', 'component/Tile'], function (Phaser, Tile) {
 
     Game.prototype = {
         preload: function () {
+            var grid_data = 'data:image/jpeg;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAYAAACM/rhtAAAABHNCSVQICAgIfAhkiAAAAFFJREFUWIXtzjERACAQBDFgMPOKzr8ScADFFlBsFKRX1WqfStLG68SNQcogZZAySBmkDFIGKYOUQcogZZAySBmkDFIGKYOUQcog9X1wJnl9ONrTcwPWLGFOywAAAABJRU5ErkJggg==',
+                background = new Image();
+
+            background.src = grid_data;
+            this.game.cache.addImage('grid', grid_data, background);
+
             this.load.image('tile', 'image/tile.png');
             this.load.image('tile2', 'image/tile2.png');
             this.load.image('tile3', 'image/tile3.png');
@@ -17,6 +23,7 @@ define(['phaser', 'component/Tile'], function (Phaser, Tile) {
         },
 
         create: function () {
+            this.game.add.tileSprite(0, 0, this.game.width, this.game.height, 'grid');
             this._spawnTiles();
         },
 
