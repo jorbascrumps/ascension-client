b4314cddfc0af5146f26a40cb88cd0d81fe5d248
iamchristopher <chriswright@live.com>
2015-08-01 22:14:52 -0400

    Added sender to chat messages

--- source/script/app/ui/Chat.js
+++ source/script/app/ui/Chat.js
@@ -4,27 +4,34 @@ define(['component/Event'], function (Event) {
     var Chat = {
         send_btn: send,
 
-        _send: function (message) {
+        _send: function (message, sender) {
             Event.emit('chat.message.send', {
-                message: message
+                message: message,
+                sender: sender
             }, true);
 
-            this._log(message);
+            this._log(message, sender);
         },
 
-        _log: function (message) {
-            log.innerHTML += '\r\n' + message;
+        _log: function (message, sender) {
+            if (!sender) {
+                sender = 'System';
+            }
+
+            log.innerHTML += '\r\n' + sender + ': ' + message;
         },
 
         initialise: function () {
             var self = this;
 
+            this._send(sender.value + ' has joined.');
+
             this.send_btn.addEventListener('click', function () {
-                self._send(message.value);
+                self._send(message.value, sender.value);
             });
 
             Event.on('chat.message.receive', function (payload) {
-                self._log(payload.message);
+                self._log(payload.message, payload.sender);
             });
         }
     };
