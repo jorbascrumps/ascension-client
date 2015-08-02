d88b8c784ff17cead864b21c5305ff708fa16edb
iamchristopher <chriswright@live.com>
2015-08-02 18:58:53 -0400

    Changed joining chat room now sends hardcoded room and user IDs

--- source/script/app/ui/Chat.js
+++ source/script/app/ui/Chat.js
@@ -3,11 +3,14 @@
 define(['component/Event'], function (Event) {
     var Chat = {
         send_btn: send,
+        room: null,
+        id: null,
 
         _send: function (message, sender) {
             Event.emit('chat.message.send', {
                 message: message,
-                sender: sender
+                sender: sender,
+                room: this.room
             }, true);
 
             this._log(message, sender);
@@ -23,19 +26,34 @@ define(['component/Event'], function (Event) {
         },
 
         initialise: function () {
-            var self = this;
+            var self = this,
+                sender = getQueryString('user');
 
-            this._send(sender.value + ' has joined.');
+            this.room = getQueryString('room');
+            this.id = getQueryString('id');
+
+            Event.emit('chat.channel.join', {
+                id: this.id,
+                room: this.room
+            }, true);
 
             this.send_btn.addEventListener('click', function () {
-                self._send(message.value, sender.value);
+                self._send(message.value, sender);
             });
 
             Event.on('chat.message.receive', function (payload) {
+                console.log('receve');
                 self._log(payload.message, payload.sender);
             });
         }
     };
 
     return Chat;
+
+    function getQueryString (field, url) {
+        var href = url ? url : window.location.href;
+        var reg = new RegExp( '[?&]' + field + '=([^&#]*)', 'i' );
+        var string = reg.exec(href);
+        return string ? string[1] : null;
+    }
 });
