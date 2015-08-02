218a6a1c057f4ab78bea30d47f39ad08d24bb885
iamchristopher <chriswright@live.com>
2015-08-02 19:32:39 -0400

    Changed event component no longer exposes socket and event objects

--- source/script/app/component/Event.js
+++ source/script/app/component/Event.js
@@ -1,21 +1,21 @@
 'use strict';
 
 define(['phaser', 'smokesignals', 'component/Socket'], function (Phaser, smokesignals, io) {
-    return {
-        _internal: smokesignals.convert({}),
-        _network: io,
+    var _internal = smokesignals.convert({}),
+        _network = io;
 
+    return {
         emit: function (event_name, payload, network_event) {
-            this._internal.emit(event_name, payload);
+            _internal.emit(event_name, payload);
 
             if (!!network_event) {
-                this._network.emit(event_name, payload);
+                _network.emit(event_name, payload);
             }
         },
 
         on: function (event_name, handler) {
-            this._internal.on(event_name, handler);
-            this._network.on(event_name, handler);
+            _internal.on(event_name, handler);
+            _network.on(event_name, handler);
         }
-    }
+    };
 });
