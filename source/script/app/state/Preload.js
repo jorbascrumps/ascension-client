3cc4a38475260af6e70ec2783b44eb4f675fa69d
iamchristopher <chriswright@live.com>
2015-08-16 14:31:05 -0400

    Added loading screen

--- source/script/app/state/Preload.js
+++ source/script/app/state/Preload.js
@@ -5,11 +5,18 @@ define(['phaser'], function (Phaser) {
 
     Preload.prototype = {
         preload: function () {
+            this.game.load.spritesheet('loading', 'image/ui/sprite/loading.GIF', 64, 64, 10);
         },
 
         create: function () {
+
+            // Display loading indicator
+            var loading = this.game.add.sprite(this.game.width - 84, this.game.height - 84, 'loading')
+              , anim_loading = loading.animations.add('load');
+            loading.animations.play('load', 10, true);
+
             this.game.physics.startSystem(Phaser.Physics.ARCADE);
-            this.game.state.start('Game');
+            this.state.game.state.start('Game');
         }
     }
 
