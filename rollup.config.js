d5e6034800be9e18c543285a70347534083b188c
Chris Wright <chris@arcane.ws>
2016-11-13 18:40:24 -0500

    Added rollup bundle process

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,24 @@
+import path from 'path';
 import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-resolve-aliases';
+
+const phaserPath = path.join(__dirname, 'node_modules/phaser');
+const phaser = path.join(phaserPath, 'build/custom/phaser-split.js');
+const pixi = path.join(phaserPath, 'build/custom/pixi.js');
+const p2 = path.join(phaserPath, 'build/custom/p2.js');
 
 export default {
     entry: 'source/index.js',
-    format: 'cjs',
+    format: 'umd',
     plugins: [
-        babel()
+        babel(),
+        resolve({
+            aliases: {
+                phaser,
+                'pixi.js': pixi,
+                p2
+            }
+        })
     ],
     dest: 'bundle.js'
 }
