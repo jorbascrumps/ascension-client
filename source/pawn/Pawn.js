7d39683f2d60f8d3416a4340fb7d30487b0c5f27
Chris Wright <chris@arcane.ws>
2016-12-13 12:07:48 -0500

    Added pawn movement

--- source/pawn/Pawn.js
+++ source/pawn/Pawn.js
@@ -1,7 +1,17 @@
+import Event from '../components/Event';
+import {
+    UPDATE_PAWN_POSITION
+} from '../components/EventTypes';
+
 export default class extends Phaser.Sprite {
     constructor ({ group, asset, position } = {}) {
         super(group.game, position.x, position.y, asset);
         group.add(this);
+
+        // Physics settings
+        this.game.physics.arcade.enable(this);
+        this.body.setSize(50, 50, 0, 0);
+        this.body.moves = false;
 
         this.tracing = false;
         this.tiles = {
@@ -16,39 +26,19 @@ export default class extends Phaser.Sprite {
     setupEvents = () => {
         this.inputEnabled = true;
         this.input.useHandCursor = true;
+        this.input.priorityID = 1;
 
         this.events.onInputDown.add(this.onInputDown);
         this.events.onInputUp.add(this.onInputUp);
         this.events.onInputOver.add(this.onInputOver);
         this.events.onInputOut.add(this.onInputOut);
         this.events.onKilled.add(this.onDeath);
+
+        Event.on(UPDATE_PAWN_POSITION, this.moveTo);
     }
 
     onInputDown = (tile, pointer) => {
         this.toggleAdjacentThings();
-
-        // const {
-        //     position: {
-        //         x: pointerX,
-        //         y: pointerY
-        //     }
-        // } = pointer;
-        // const {
-        //     game: {
-        //         camera: {
-        //             x: cameraX,
-        //             y: cameraY
-        //         }
-        //     }
-        // } = this;
-        // const newPosition = {
-        //     x: pointerX + cameraX,
-        //     y: pointerY + cameraY
-        // };
-        //
-        // this.game.add.tween(this)
-        //     .to(newPosition, 0, Phaser.Easing.Linear.Noce)
-        //     .start()
     }
 
     onInputUp = () => {
@@ -71,6 +61,8 @@ export default class extends Phaser.Sprite {
     }
 
     traceAdjacentTiles = () => {
+        this.clearAdjacentTiles();
+
         const positions = [
             { x : this.position.x - 50, y: this.position.y + 50 }, // bottom left
             { x : this.position.x - 50, y: this.position.y      }, // left
@@ -91,6 +83,7 @@ export default class extends Phaser.Sprite {
         sprite.height = 50;
         sprite.width = 50;
         sprite.inputEnabled = true;
+        this.game.physics.arcade.enable(sprite);
         this.tiles.collisions.add(sprite);
 
         const overlay = this.game.add.graphics();
@@ -104,5 +97,33 @@ export default class extends Phaser.Sprite {
 
     toggleAdjacentThings = () =>  {
         this.tiles.traces.forEach(thing => thing.exists = !thing.exists);
+    }
+
+    clearAdjacentTiles = () => {
+        this.tiles.collisions.removeChildren();
+        this.tiles.traces.removeChildren();
+    }
+
+    update = () => {}
+
+    moveTo = ({ x, y }) => {
+        const newPos = {
+            x: x - (x % 50),
+            y: y - (y % 50)
+        };
+        const distance = Math.floor(new Phaser.Line(
+            this.position.x,
+            this.position.y,
+            newPos.x,
+            newPos.y
+        ).length);
+        const duration = (distance - (distance % 50)) * 10;
+
+        this.game.add.tween(this)
+            .to(newPos, duration, Phaser.Easing.Linear.None)
+            .start()
+            .onComplete.add((pawn, tween) => {
+                this.traceAdjacentTiles();
+            });
     }
 }
