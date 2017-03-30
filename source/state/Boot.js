1f9f64fd3845bb9861c2f63dcd8e6838d27d2559
Chris Wright <chris@arcane.ws>
2017-03-29 20:35:53 -0400

    Changed game no longer stops rendering when focus is lost

--- source/state/Boot.js
+++ source/state/Boot.js
@@ -4,6 +4,7 @@ export default class {
     }
 
     create () {
+        this.game.disableVisibilityChange = true;
         this.input.maxPointers = 1;
         this.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
         this.scale.pageAlignHorizontally = true;
