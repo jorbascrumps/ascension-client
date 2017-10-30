350c1559c485d23992ab0f1addb043fd890d7a44
iamchristopher <chriswright@live.com>
2017-10-30 19:52:57 -0400

    Updated Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,22 @@
-import path from 'path';
 import babel from 'rollup-plugin-babel';
-import alias from 'rollup-plugin-resolve-aliases';
+import builtins from 'rollup-plugin-node-builtins';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
-import replace from 'rollup-plugin-replace';
-import builtins from 'rollup-plugin-node-builtins';
-
-const phaserPath = path.join(__dirname, 'node_modules/phaser');
-const phaser = path.join(phaserPath, 'build/custom/phaser-split.js');
-const pixi = path.join(phaserPath, 'build/custom/pixi.js');
-const p2 = path.join(phaserPath, 'build/custom/p2.js');
 
 export default {
-    entry: 'source/index.js',
-    format: 'umd',
+    input: 'source/index.js',
+    output: {
+        file: 'bundle.js',
+        format: 'umd'
+    },
     plugins: [
         babel(),
+        builtins(),
         commonjs(),
         resolve({
             jsnext: true,
             main: true,
             browser: true
-        }),
-        alias({
-            aliases: {
-                phaser,
-                'pixi.js': pixi,
-                p2
-            }
-        }),
-        replace({
-            'process.env.NODE_ENV': JSON.stringify('development')
-        }),
-        builtins()
-    ],
-    dest: 'bundle.js'
-}
+        })
+    ]
+};
