638728060d335f198a85e22341c10f07b1dc7ed3
iamchristopher <chriswright@live.com>
2018-05-21 18:48:03 -0400

    Added replace value for process.env.ENV

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import babel from 'rollup-plugin-babel';
 import builtins from 'rollup-plugin-node-builtins';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
+import replace from 'rollup-plugin-replace';
 
 export default {
     input: 'source/index.js',
@@ -17,6 +18,9 @@ export default {
             jsnext: true,
             main: true,
             browser: true
+        }),
+        replace({
+            'process.env.NODE_ENV': JSON.stringify('development')
         })
     ]
 };
