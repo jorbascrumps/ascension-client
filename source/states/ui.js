7561b7e61790abf27caa189995fba635cb3d0761
iamchristopher <chriswright@live.com>
2018-06-25 20:10:08 -0400

    Added current phase to UI

--- source/states/ui.js
+++ source/states/ui.js
@@ -1,3 +1,31 @@
 export const key = 'UI';
 
-export function create () {}
+let cachedPhase;
+
+export function create () {
+    this.currentPhase = this.add.text(10, 10, '', {
+        fontSize: 10,
+        fontFamily: 'Arial'
+    });
+}
+
+export function update () {
+    const {
+        client: {
+            store: {
+                getState
+            }
+        }
+    } = this.scene.get('LEVEL');
+    const {
+        G,
+        ctx
+    } = getState();
+
+    if (ctx.phase === cachedPhase) {
+        return;
+    }
+
+    cachedPhase = ctx.phase;
+    this.currentPhase.setText(`Phase: ${ctx.phase}`);
+}
