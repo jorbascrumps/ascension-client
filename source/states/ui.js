c467c97b4d56420d657b1a1c93a537360d0d05e3
iamchristopher <chriswright@live.com>
2019-01-27 16:19:35 -0500

    Replaced ActionBar plugin with list view implementation

--- source/states/ui.js
+++ source/states/ui.js
@@ -3,21 +3,6 @@ export const key = 'UI';
 let cachedPhase;
 
 export function create () {
-    this.actionBar.start(
-        this.cameras.main.centerX,
-        this.cameras.main.height,
-        ({ target: { title: action } }) => {
-            if ('' === action) {
-                return;
-            }
-
-            if (action === 'Cancel') {
-                return window.client.events.endPhase('Activation');
-            }
-
-            window.client.events.endPhase(action);
-        }
-    );
     this.currentPhase = this.add.text(10, 10, '', {
         fontSize: 10,
         fontFamily: 'Arial'
@@ -37,6 +22,24 @@ export function create () {
                 .setPosition(this.cameras.main.width / 2, 25)
                 .setName('label')
         ]);
+    this.input.topOnly = false;
+
+    this.actions = this.add.listview(10, 30, 150, 300)
+        .on('pointerover', item => item.setAlpha(1))
+        .on('pointerout', item => item.setAlpha(0.5))
+        .on('pointerdown', (item, i) => {
+            item.setAlpha(0.75)
+
+            let next = item.getByName('action').text;
+            if (next === 'Cancel') {
+                next = 'Activation';
+            }
+
+            window.client.events.endPhase({
+                next,
+            });
+        })
+        .on('pointerup', item => item.setAlpha(1));
 }
 
 function onChangeData (_, key, val) {
@@ -75,8 +78,9 @@ export function update () {
     const {
         ctx
     } = getState();
+
     if (ctx.currentPlayer !== currentPlayer.id) {
-        this.actionBar.setActions();
+        this.actions.clear()
         return this.currentPhase.setText('');
     }
 
@@ -86,9 +90,34 @@ export function update () {
 
     cachedPhase = ctx.phase;
     this.currentPhase.setText(`Phase: ${ctx.phase}`)
-        .setFixedSize(2000, 0);;
+        .setFixedSize(2000, 0);
 
-    this.actionBar.setActions(actions[ctx.phase]);
+    const actionsControls = actions[ctx.phase]
+        .map(action => {
+            const text = this.add.text(0, 0, action, {
+                fontSize: 20,
+                fontFamily: 'Arial'
+            })
+                .setName('action')
+                .setPadding({
+                    bottom: 6,
+                    left: 8,
+                    right: 8,
+                    top: 8
+                });
+
+            return this.add.container(0, 0)
+                .add([
+                    this.add.rectangle(0, 0, 150, text.displayHeight, 0x6666ff)
+                        .setOrigin(0, 0),
+                    text,
+                ])
+                .setAlpha(0.5)
+        });
+
+    this.actions
+        .clear()
+        .add(actionsControls)
 }
 
 const actions = {
